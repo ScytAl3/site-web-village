f30000e166b2f7b822987ed76ade497b36a35c4d
ScytAl3 <45272552+ScytAl3@users.noreply.github.com>
2019-05-10 11:15:21 +0200

    feat: update templates for events & logout

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -12,11 +12,18 @@ class UserFixtures extends Fixture
 
     private $pwdEncoder;
 
+    /**     
+     * @param UserPasswordEncoderInterface $pwdEncoder
+     */
     public function __construct(UserPasswordEncoderInterface $pwdEncoder)
     {
         $this->pwdEncoder = $pwdEncoder;
     }
 
+    /**
+     * @param ObjectManager $manager
+     * @return User
+     */
     public function load(ObjectManager $manager)
     {
         $user = new User();
