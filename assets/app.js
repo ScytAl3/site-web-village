5b7b4b09fa8abbc7a05558bddeb0ab6b66a195b8
ScytAl3 <45272552+ScytAl3@users.noreply.github.com>
2021-07-07 13:29:47 +0200

    Update app.js, change app.css to scss and add map.css

--- assets/app.js
+++ assets/app.js
@@ -6,8 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-import './styles/global.scss';
+import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
@@ -17,4 +16,3 @@ const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
-
